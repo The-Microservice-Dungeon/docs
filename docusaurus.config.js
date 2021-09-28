0ac9c9944db7115fb9d3b5cb18c1da7d0c9903bf
Daniel Khaapamyaki <khaapamyaki@gmail.com>
2021-09-28 23:02:10 +0200

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,104 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "The Microservice Dungeon",
+  tagline: "WS 21/21",
+  url: "https://The-Microservice-Dungeon.github.io",
+  baseUrl: "/docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "The-Microservice-Dungeon", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/The-Microservice-Dungeon/docs/edit/main/",
+          path: "docs",
+          routeBasePath: "/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/The-Microservice-Dungeon/docs/edit/main/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "The Microservice Dungeon",
+        logo: {
+          alt: "Spaceship",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/The-Microservice-Dungeon",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/The-Microservice-Dungeon",
+              },
+              {
+                label: "Archi Lab",
+                href: "https://archi-lab.io/display/public/The+Microservice+Dungeon",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Archi Lab. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
