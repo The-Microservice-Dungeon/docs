5add77a96fc64e1088aaa57ad362dc673d05581d
Daniel Khaapamyaki <daniel.khaapamyaki@smail.th-koeln.de>
2021-12-03 18:49:22 +0000

    Deploy website - based on 623bcb43ffdfa51cb92e2d30303abf9b9d16604e

--- assets/js/cc36e012.3054b05d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2873],{62685:function(e){e.exports=JSON.parse('{"specUrl":"https://raw.githubusercontent.com/The-Microservice-Dungeon/robot/main/swagger/v4/swagger.yaml","type":"url","content":"https://raw.githubusercontent.com/The-Microservice-Dungeon/robot/main/swagger/v4/swagger.yaml"}')}}]);
